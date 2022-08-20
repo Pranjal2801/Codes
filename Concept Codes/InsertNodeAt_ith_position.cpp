7a858cd03eafe468f398b317263a8eeb29a306d6
Pranjal <85683350+Pranjal2801@users.noreply.github.com>
2022-08-20 14:54:37 +0530

    Update InsertNodeAt_ith_position.cpp

--- Concept Codes/InsertNodeAt_ith_position.cpp
+++ Concept Codes/InsertNodeAt_ith_position.cpp
@@ -1,13 +1,23 @@
-void insertnode(Node* head, int i, int data){
+Node* insertnode(Node* head, int i, int data){
+    Node* addnewnode = new Node(data);
     int count = 0;
     Node* temp = head;
-    while(count <= i - 1){
-        
-        temp = temp -> nextadd;
+    
+    if(i == 0){
+        //Node* addnewnode = new Node(data);
+        addnewnode -> nextadd = head;
+        head = addnewnode;
+        return head;
     }
     
-    Node* addnewnode = new Node(data);
-    addnewnode -> nextadd = temp -> nextadd;
-    temp -> nextadd = addnewnode;
-        
+    while(temp != NULL && count < i - 1){
+        temp = temp -> nextadd;
+        count++;
+    }
+    
+    if(temp != NULL){
+        addnewnode -> nextadd = temp -> nextadd;
+        temp -> nextadd = addnewnode;
+    }
+    return head;
 }
